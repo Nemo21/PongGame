1ee46e4ce754fe504c5f01d5d4c1215c5fd475f7
Nemo21 <saggubableen9@gmail.com>
2025-02-01 17:36:21 +0530

    Resolution changes

--- README.md
+++ README.md
@@ -27,3 +27,47 @@ function love.draw()
     love.graphics.printf("Hello pong!",0,WINDOW_HEIGHT/2-6,WINDOW_WIDTH,"center")
 end
 ```
+
+## Lesson 1:
+- Till now we have created a sample Love2D app which says hello pong.
+- Problem: 
+    - Resolution problem: 
+        - The text and images are blurry by default
+        - we need low res for more retro feel
+- Solution: 
+    - push is a library that will allow us to draw our game at a virtual resolution, 
+    - instead of however large our window is used to provide a more retro aesthetic 
+    - use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text and graphics
+- love.graphics.setDefaultFilter is a function for texture scaling which by default is bilinear
+
+```
+love.graphics.setDefaultFilter("nearest","nearest")
+```
+- Function provided by push library to render our application in a virutal resolution 
+```
+push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_HEIGHT,WINDOW_WIDTH,{
+        fullscreen=false,
+        resizable=false,
+        vsync=true
+    })
+```
+- Key event handling called by Love2D in each frame
+```
+function love.keypressed(key)
+    --[[key accessed by string name]]
+    if key=='escape' then
+        --[[function by Love2D to terminate application]]
+        love.event.quit()
+    end
+end
+```
+```
+function love.draw()
+    --[[begin rendering with virtual resolution]]
+    push:apply('start')
+    --[[use virtual width and height,everything written inside the push:apply block will be rendered in virtual resolution]]
+    love.graphics.printf("Hello pong!",0,VIRTUAL_HEIGHT/2-6,VIRTUAL_WIDTH,"center")
+    --[[end rendering at virtual resolution]]
+    push:apply('end')
+end
+```
--- main.lua
+++ main.lua
@@ -2,8 +2,20 @@
     Remaking Pong in 2025
     TIP1:Read the damn error for fucks sake
         >it was a spelling mistake in resizable bruh
+    PROBLEM 1:
+        Resolution problem: 
+            The text and images are blurry by default
+            we need low res for more retro feel
 ]]
 
+
+--[[
+   push is a library that will allow us 
+   to draw our game at a virtual resolution, 
+   instead of however large our window is; 
+   used to provide a more retro aesthetic 
+]]
+push = require 'push'
 --[[
     Set window height and window width 
     which will be used throughout 
@@ -12,16 +24,47 @@ WINDOW_WIDTH=1280
 WINDOW_HEIGHT=720
 
 --[[
+    initialize our virtual resolution, 
+    which will be rendered within our
+    actual window no matter its dimensions
+]]
+VIRTUAL_WIDTH=432
+VIRTUAL_HEIGHT=243
+
+--[[
     Function run when game first starts up
     and only once its run to initailize game
 ]]
 
 function love.load()
-    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+--[[
+    use nearest-neighbor filtering on upscaling and downscaling 
+    to prevent blurring of text and graphics
+    try removing this function to see the difference!
+]]
+    love.graphics.setDefaultFilter("nearest","nearest")
+    --[[
+        function provided by the push library
+        check push.lua for more info
+    ]]
+    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_HEIGHT,WINDOW_WIDTH,{
         fullscreen=false,
         resizable=false,
         vsync=true
     })
+end
+
+
+--[[
+    Key event handling called by Love2D in each frame
+]]
+
+function love.keypressed(key)
+    --[[key accessed by string name]]
+    if key=='escape' then
+        --[[function by Love2D to terminate application]]
+        love.event.quit()
+    end
 end
 
 --[[
@@ -30,7 +73,12 @@ end
 ]]
 
 function love.draw()
-    love.graphics.printf("Hello pong!",0,WINDOW_HEIGHT/2-6,WINDOW_WIDTH,"center")
+    --[[begin rendering with virtual resolution]]
+    push:apply('start')
+    --[[use virtual width and height]]
+    love.graphics.printf("Hello pong!",0,VIRTUAL_HEIGHT/2-6,VIRTUAL_WIDTH,"center")
+    --[[end rendering at virtual resolution]]
+    push:apply('end')
 end
 --[[
     printf("text to render",starting X,starting Y,number of pixels to center within,alignment mode)
